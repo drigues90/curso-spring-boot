20cb5fbd496279072708f627598e18202f21d4ad
jonathan <jonathan@jonathan.com>
2021-07-25 20:29:44 -0300

    inicio da implementacao de segurança

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$G3D6CJJEfqsoj5Px5zCi7uUjSPFe3RCFYAjZEYz3kDAxTrWsNn9Ri');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
